8a2ca33a7caf509ef132d5bd3b758cce248fc1bf
abhik47 <abhishek9817@gmail.com>
2020-07-15 11:39:00 +0530

    changed

--- app.js
+++ app.js
@@ -1,27 +1,93 @@
-(function(){
-          'use strict';
-          angular.module("LunchCheck",[]).controller("LunchCheckController",myCtrl);
+(function () {
+  'use strict';
+  
+  angular.module("ShoppingListCheckOff",[])
+  .controller("ToBuyController",ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
+  
 
-          function myCtrl($scope){
-            $scope.name = "";
-            $scope.output1= "";
-            $scope.output2= "";
-            $scope.cal = function(){
-                $scope.output1= "";
-                $scope.output2= "";
-              let str = $scope.name;
-              let word = str.split(",");
-              if(str == ""){
-                $scope.output1 = "Please enter data first";
-              }
-              else if(word.length <= 3){
-                $scope.output2 = "Enjoy!";
-              }
-              else{
-                $scope.output2 = "Too much!";
-              }
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
+    toBuy.count = ShoppingListCheckOffService.count;
+    toBuy.items =  [
+    {
+      name : 'cookiee',
+      quantity:'20'
 
-            }
-          };
+    },
+    {
 
-        })();
+      name : 'Cashu',
+      quantity:'20'
+    },
+    {
+
+      name : 'biscuit',
+      quantity:'20'
+    },
+    {
+
+      name : 'orio',
+      quantity:'20'
+    },
+    {
+      name : 'svs',
+      quantity:'20'
+
+    }
+
+    ];
+
+    ShoppingListCheckOffService.setItem(toBuy.items);
+
+    toBuy.bought = function(index){
+        
+        toBuy.items = ShoppingListCheckOffService.update(index);
+        toBuy.count = ShoppingListCheckOffService.count;
+            
+      }
+     
+    };
+    
+
+
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+
+    var showBought = this;
+    showBought.items = ShoppingListCheckOffService.getItem();
+  };
+
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+    var items = [];
+    var items2 = [];
+
+    service.count = 5;
+
+    service.setItem = function(ObjLiteral){
+      items = ObjLiteral;
+    }
+
+    service.getItem = function(){
+      
+      return items2;
+    }
+
+    
+
+    service.update = function(itemIndex){
+      items2.push(items[itemIndex])
+      items.splice(itemIndex,1);
+      service.count -= 1; 
+           
+      return items;
+    }
+  };
+
+})();
